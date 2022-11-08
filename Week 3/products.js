bbd55efdd7de985e41bc74b6c13fd243a825ca4a
Khadija Shaikh <khadijah704shaikh@gmail.com>
2022-11-08 09:51:42 +0400

    rytfghg

---
+++ Week 3/products.js
@@ -0,0 +1,57 @@
+let products =[
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description:
+          "A 25 pound bag of <em>irresistible</em>," +
+          " organic goodness for your cat.",
+        price: 2000,
+        image: "Images/catFood.jpg_480Wx480H",
+        availableInventory: 10,
+        rating:3
+      },
+      {
+        id: 1002,
+        title: "Cat Food, 25lb bag",
+        description:
+          "A 25 pound bag of <em>irresistible</em>," +
+          " organic goodness for your cat.",
+        price: 2100,
+        image: "Images/2.jpg",
+        availableInventory: 4,
+        rating:4
+      },
+      {
+        id: 1003,
+        title: "Cat Food, 25lb bag",
+        description:
+          "A 25 pound bag of <em>irresistible</em>," +
+          " organic goodness for your cat.",
+        price: 2500,
+        image: "Images/3.jpg",
+        availableInventory: 8,
+        rating:2
+      },
+      {
+        id: 1004,
+        title: "Cat Food, 25lb bag",
+        description:
+          "A 25 pound bag of <em>irresistible</em>," +
+          " organic goodness for your cat.",
+        price: 900,
+        image: "Images/4.jpg",
+        availableInventory: 8,
+        rating:1
+      },
+      {
+        id: 1005,
+        title: "Cat Food, 25lb bag",
+        description:
+          "A 25 pound bag of <em>irresistible</em>," +
+          " organic goodness for your cat.",
+        price: 5000,
+        image: "Images/5.jpg",
+        availableInventory: 1,
+        rating:5
+      }
+]
